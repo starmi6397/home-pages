a4a519378acdb741c5c9e7dbca60e51db51be1f5
starmi <zhangzijian2222@outlook.com>
2024-11-30 15:41:40 +0800

    修改歌单和“more-content”

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">丁达尔效应出现的时候，光便有了形状，而当你出现时，心动便有了定义；光可以治愈万物，就像有你出现的世界，就是你给我的宇宙级浪漫。</div>
 </template>
 
 <style lang="scss" scoped>
